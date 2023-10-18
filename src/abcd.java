9146bf44aeef1a6b3262e1a541d0c590e0e1d375
avani244@ <patelavani244@gmail.com>
2023-10-18 16:54:00 +0100

    updated

---
+++ src/abcd.java
@@ -0,0 +1,10 @@
+public class abcd {
+    public static void main(String[] args) {
+        System.out.println("Name: Avani");
+        System.out.println("Name: Shikha");
+        System.out.println("Name: Khusboo");
+        System.out.println("Name: Kinjal");
+        System.out.println("Name: Ripti");
+        System.out.println("Name: Nayan:");
+    }
+}
